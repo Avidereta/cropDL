14cd606e7129b3b5487a985d0e8e7af380352f50
Макарова Анастасия <makarova@head.control>
2016-11-01 22:06:21 +0300

    ADD: config example

---
+++ config.py
@@ -0,0 +1,7 @@
+""" path to save results (weights, scores, some plots"""
+results_path = "/home/makarova/columbia/code/results/"
+
+""" paths to txt files where each line - path to ground_truth image and the input image"""
+txt_train = '/home/makarova/columbia/data/inoculated_1/gt_img_train_inoc.txt'
+txt_valid = '/home/makarova/columbia/data/inoculated_1/gt_img_valid_inoc.txt'
+txt_test = '/home/makarova/columbia/data/inoculated_1/gt_img_test_inoc.txt'
